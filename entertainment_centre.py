e3910c720575a2a86e955eaaf995f4081dc0ccb7
kwanjas3 <kwanjas0412@gmail.com>
2017-09-15 20:08:02 -0400

    added framework freshtomatoes for html site generation

--- entertainment_centre.py
+++ entertainment_centre.py
@@ -1,12 +1,11 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story", "Boy and his Toys"
                         , "http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v8_ab.jpg"
                         , "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-
-print(toy_story.storyline)
-toy_story.show_trailer()
-
+movies = [toy_story]
+fresh_tomatoes.open_movies_page(movies)
 
 
     
